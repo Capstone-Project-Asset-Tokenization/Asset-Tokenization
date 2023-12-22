65759930670197b59f6995fbde29a4ecde2f5663
GetnetAdugna <getnetadugna25@gmail.com>
2023-12-22 11:34:58 +0300

    Registration\Getnet - adding features on the project structure

--- frontend/README.md
+++ frontend/README.md
@@ -1,3 +1,5 @@
+# Asset-Tokenization Platform
+
 # React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import tailwindcss from 'tailwindcss'
 
 export default defineConfig({
   plugins: [
     react(),
     reactRefresh(),
+    require('tailwindcss'),
   ],
   css: {
     postcss: {
