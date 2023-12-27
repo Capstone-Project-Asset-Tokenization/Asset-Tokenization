0d5908f99034d2b99ee4f72c584f91881b461037
Jebessa Dejene <jebessadejene2021@gmail.com>
2023-12-27 11:06:52 +0300

    check label working

--- frontend/src/pages/AssetRegistration.jsx
+++ frontend/src/pages/AssetRegistration.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AssetRegistration() {
   return (
 <div className="container mx-auto p-4">
-    <h1 className="text-3xl font-bold mb-4">Hello, Asset AssetRegistration Page!</h1>
+    <h1 className="text-3xl font-bold mb-4">Hello, Asset Asset Registration Page!</h1>
     
   </div>
   )
