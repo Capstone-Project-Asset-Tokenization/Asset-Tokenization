92672ad75f8993afdeda6a75656e97a80cfd4c35
Jebessa Dejene <jebessadejene2021@gmail.com>
2024-01-01 20:23:36 +0300

    Add landing page,routing, and global css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import LandingPage from "./pages/LandingPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Navbar from "./components/layout/Navbar";
 import AssetRegistration from "./pages/AssetRegistration";
+import UserRegistration from "./pages/userRegistration";
 // import Footer from "./components/layout/Footer";
 
 const router = createBrowserRouter(
@@ -16,16 +17,17 @@ const router = createBrowserRouter(
     <Route>
       <Route index element={<LandingPage />} />
       <Route path="/asset-registration" element={<AssetRegistration />} />
+      <Route path="/signup" element={<UserRegistration />} />
     </Route>
   )
 );
 
 function App({ routes }) {
   return (
-    <>
+    <div className="">
       <Navbar />
       <RouterProvider router={router} />
-    </>
+    </div>
   );
 }
 
