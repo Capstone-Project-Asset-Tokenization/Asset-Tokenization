994bd8d524c6baf6a2bae00b5e0f177b998efa3e
GemechisUrgessa <gemechis.urgessa@aait.edu.et>
2024-03-31 00:22:52 +0300

    added some fields and some moodifications

--- blockchian/contracts/UserRegistration.sol
+++ blockchian/contracts/UserRegistration.sol
@@ -3,23 +3,25 @@ pragma solidity >=0.7.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
+// UserRegistration contract allows for the registration, update, and management of user details.
 contract UserRegistration {
     using SafeMath for uint256;
-
+    // Struct representing a user with various attributes.
     struct User {
         uint256 userId; // Unique user identifier
-        string fullName; // Full name of the user
-        string password; // Password for the user
+        string firstName; // First name of the user
+        string lastName; // Last name of the user
         string email; // Email address of the user
-        bool isRegistered; // Indicates if the user is registered
         bool isSuspended; // incase the user gets suspended
-        string userType; // Type of the user
+        bool isGovermentOfficial; // Indicates if the user is a government official
         address owner; // Owner of the user
         string legalIdNo; // Unique legal identifier for user
+        bool isUserDeleted; // Indicates if the user is deleted, soft delete
     }
 
     mapping(uint256 => User) public users;
     mapping(string => uint256) public userIdsByLegalIdNo;
+    mapping(address => User) public usersByAddress;
     uint256 private nextUserId = 1;
 
     event UserRegistered(uint256 userId, string legalIdNo, address owner);
@@ -27,60 +29,62 @@ contract UserRegistration {
     event UserDeleted(uint256 userId, string legalIdNo);
     event UserSuspended(uint256 userId, string legalIdNo);
 
-    function registerUser(string memory _fullName, string memory _password, string memory _email, string memory _userType, string memory _legalIdNo) public {
+    // Allows a user to be registered in the system with provided details.
+    function registerUser(string memory _firstName,
+     string memory _lastName,
+      string memory _email,
+       bool _isGovermentOfficial,
+        string memory _legalIdNo,
+        address _owner)
+         public {
         require(userIdsByLegalIdNo[_legalIdNo] == 0, "User with this legal ID already exists.");
-
+        require(usersByAddress[_owner].owner == address(0), "User with this address already exists.");
         User memory newUser = User({
             userId: nextUserId,
-            fullName: _fullName,
-            password: _password,
+            firstName: _firstName,
+            lastName: _lastName,
             email: _email,
-            isRegistered: true,
             isSuspended: false,
-            userType: _userType,
-            owner: msg.sender,
-            legalIdNo: _legalIdNo
+            isGovermentOfficial: _isGovermentOfficial,
+            owner: _owner,
+            legalIdNo: _legalIdNo,
+            isUserDeleted: false
         });
-
         users[nextUserId] = newUser;
         userIdsByLegalIdNo[_legalIdNo] = nextUserId;
-
-        emit UserRegistered(nextUserId, _legalIdNo, msg.sender);
-
+        usersByAddress[_owner] = newUser;
+        emit UserRegistered(nextUserId, _legalIdNo, _owner);
         nextUserId = nextUserId.add(1);
     }
 
-    function updateUserInfo(uint256 _userId, string memory _fullName, string memory _email, string memory _userType) public {
-        require(users[_userId].isRegistered, "User not registered.");
-        require(users[_userId].owner == msg.sender, "Unauthorized.");
+    // Allows the owner of a user to update the user's information.
+     function updateUserInfo(uint256 _userId,
+     string memory _firstName,
+      string memory _lastName,
+      string memory _email,
+       address _owner
+       ) public {
+        require(users[_userId].owner == _owner, "Unauthorized.");
         require(!users[_userId].isSuspended, "User is suspended!");
-
         User storage user = users[_userId];
-        user.fullName = _fullName;
+        user.firstName = _firstName;
+        user.lastName = _lastName;
         user.email = _email;
-        user.userType = _userType;
-
-        emit UserUpdated(_userId, user.legalIdNo, msg.sender);
+        emit UserUpdated(_userId, user.legalIdNo, _owner);
     }
 
-    function deleteUser(uint256 _userId) public {
-        require(users[_userId].isRegistered, "User not registered.");
-        require(users[_userId].owner == msg.sender, "Unauthorized.");
-
+    function deleteUser(uint256 _userId, address _officalsAddress
+    ) public {
+        require(users[usersByAddress[_officalsAddress].userId].isGovermentOfficial, "Unauthorized.");
+        users[_userId].isUserDeleted = true;
         emit UserDeleted(_userId, users[_userId].legalIdNo);
 
-        delete userIdsByLegalIdNo[users[_userId].legalIdNo];
-        delete users[_userId];
     }
 
-    function suspendUser(uint256 _userId) public {
-        require(users[_userId].isRegistered, "User not registered.");
-        require(users[_userId].owner == msg.sender, "Unauthorized.");
-
+    function suspendUser(uint256 _userId, address _officalsAddress
+    ) public {
+        require(users[usersByAddress[_officalsAddress].userId].isGovermentOfficial, "Unauthorized.");
         users[_userId].isSuspended = true;
-
         emit UserSuspended(_userId, users[_userId].legalIdNo);
     }
-
-
 }
