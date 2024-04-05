bfbdd81305d2d7996f8c06aba825fd1254e4c700
Jebessa Dejene <jebessadejene2021@gmail.com>
2024-04-05 23:53:31 +0300

    Modification on navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,95 +1,101 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai"; // For menu icons
 import { IoRocketOutline } from "react-icons/io5";
-import { useSelector, useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  let dispatch = useDispatch()
+  let dispatch = useDispatch();
 
-  const authState = useSelector(state => state.auth);
+  const authState = useSelector((state) => state.auth);
 
   const initialUrls = [
     {
-      name: 'Home',
-      link: '/',
-      protected: false
+      name: "Home",
+      link: "/",
+      protected: false,
     },
     {
-      name: 'Marketplace',
-      link: '/asset-marketplace',
-      protected: false
-
+      name: "Marketplace",
+      link: "/asset-marketplace",
+      protected: false,
     },
     {
-      name: 'Register Asset',
-      link: '/asset-registration',
-      protected: true
+      name: "Register Asset",
+      link: "/asset-registration",
+      protected: true,
     },
     {
-      name: 'Verify Asset',
-      link: '/asset-verification',
-      protected: true
+      name: "Verify Asset",
+      link: "/asset-verification",
+      protected: true,
     },
     {
-      name: 'Login',
-      link: '/signin',
-      protected: false
+      name: "Login",
+      link: "/signin",
+      protected: false,
     },
     {
-      name: 'Sign Up',
-      link: '/signup',
-      protected: false
+      name: "Sign Up",
+      link: "/signup",
+      protected: false,
     },
     {
-      name: 'Profile',
-      link: '/profile',
-      protected: true
+      name: "Profile",
+      link: "/profile",
+      protected: true,
     },
     {
-      name: 'LogOut',
-      link: '/',
+      name: "LogOut",
+      link: "/",
       protected: true,
       onClick: () => {
         dispatch(logout());
-      }
-    }
-  ]
+      },
+    },
+  ];
 
   const [navbarItems, setNavbarItems] = useState(initialUrls);
 
   useEffect(() => {
     if (authState.isAuthenticated) {
-      let temp = initialUrls.filter(item => item.name !== 'Login' && item.name !== 'Sign Up');
+      let temp = initialUrls.filter(
+        (item) => item.name !== "Login" && item.name !== "Sign Up"
+      );
       setNavbarItems(temp);
     } else {
-      let temp = initialUrls.filter(item => !item.protected);
+      let temp = initialUrls.filter((item) => !item.protected);
       setNavbarItems(temp);
     }
   }, [authState.isAuthenticated]);
 
   return (
-    <nav className=" shadow-lg text-white border-b-1.7 p-3">
+    <nav className=" shadow-lg text-white border-b-1.7 ">
       <div className="mx-4">
         <div className="flex justify-between">
-          <div className="flex ">
-            <div>
-              {/* Website Logo */}
-              <Link to="/" className="flex items-center py-4 px-2">
-                <span className="font-semibold  text-lg">
-                  Asset Tokenization Platform
-                </span>
-              </Link>
+          <Link to="/" className="p-1">
+            <div className="w-36 object-contain">
+              <img
+                className=""
+                src=" ../src/assets/ATPlogo.png"
+                alt="clip image"
+              />
             </div>
-            {/* Primary Navbar items */}
-          </div>
-          <div className="hidden md:flex items-center ">
-            {
-              navbarItems.map((item, index) => (
-                <Link key={index} to={item.link} onClick={() => { if (item.onClick) item.onClick() }} className="py-2 px-2 m-2 hover:bg-purple-500 transition duration-300  rounded font-semibold">{item.name}</Link>
-              ))
-            }
+          </Link>
 
+          <div className="hidden md:flex items-center ">
+            {navbarItems.map((item, index) => (
+              <Link
+                key={index}
+                to={item.link}
+                onClick={() => {
+                  if (item.onClick) item.onClick();
+                }}
+                className="py-2 px-2 m-2 hover:bg-purple-500 transition duration-300  rounded font-semibold"
+              >
+                {item.name}
+              </Link>
+            ))}
           </div>
           {/* Secondary Navbar items */}
           {/* Mobile menu button */}
@@ -108,12 +114,16 @@ const Navbar = () => {
         </div>
       </div>
       {/* Mobile Menu */}
-      <div className={`md:hidden ${isMenuOpen ? 'block' : 'hidden'}`}>
-        {
-          navbarItems.map((item, index) => (
-            <Link key={index} to={item.link} className="block py-2 px-4 text-sm hover:bg-purple-500 hover:text-white">{item.name}</Link>
-          ))
-        }
+      <div className={`md:hidden ${isMenuOpen ? "block" : "hidden"}`}>
+        {navbarItems.map((item, index) => (
+          <Link
+            key={index}
+            to={item.link}
+            className="block py-2 px-4 text-sm hover:bg-purple-500 hover:text-white"
+          >
+            {item.name}
+          </Link>
+        ))}
       </div>
     </nav>
   );
