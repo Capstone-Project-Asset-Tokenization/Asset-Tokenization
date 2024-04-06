7a988e6395a668095a12e0675c4afc6b9185fd5b
zeamanual <zeamanualfeleke@gmail.com>
2024-04-06 10:05:06 +0300

    Auth fix

--- backend/app/controllers/auth_controller.js
+++ backend/app/controllers/auth_controller.js
@@ -44,6 +44,8 @@ export const LoginController = async (req, res) => {
 
     const user = await User.findOne({ email: email });
 
+    if (!user) return res.status(400).send("User not found");
+
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) return res.status(400).send("Invalid password");
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
