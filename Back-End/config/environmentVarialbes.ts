f8c43ead432ae8c5832aeea354fe62162c264bc9
GemechisUrgessa <gemechis.urgessa@aait.edu.et>
2024-04-14 16:27:35 +0300

    some changes made

--- Back-End/config/environmentVarialbes.ts
+++ Back-End/config/environmentVarialbes.ts
@@ -4,8 +4,8 @@ export const EnvConfig ={
     MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/asset-tokenization',
     JWT_SECRET: process.env.JWT_SECRET || 'something',
     CLOUD_NAME: process.env.CLOUD_NAME || 'drjsgceky',
-    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || '828644661493838',
-    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || '***************************',
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY || "342699666849659",
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET || "QMY5rZRJ8p9zgpXGh7G4Xr8pryI",
     EMAIL_USER: process.env.EMAIL_USER || 'asset',
     EMAIL_PASSWORD: process.env.EMAIL_PASSWORD || '**********',
 }
