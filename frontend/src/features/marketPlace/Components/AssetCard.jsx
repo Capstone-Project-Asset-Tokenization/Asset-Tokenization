2179feb1de94a1097f4b229d002edfaf59f51900
petros beyene <petrosbeyene611@gmail.com>
2024-04-15 10:02:14 +0300

    Implement asset fetching and search functionality

--- frontend/src/features/marketPlace/Components/AssetCard.jsx
+++ frontend/src/features/marketPlace/Components/AssetCard.jsx
@@ -1,34 +1,28 @@
 import React from "react";
 
-// import assetImage from 
-
-const AssetCard = ({ assetImage, assetName, owner, price }) => {
+const AssetCard = ({ name, totalSupply, tokenPrice, images }) => {
     return (
-        <div className="flex flex-col shadow-lg rounded-[20px] overflow-hidden">
+        <div className="flex flex-col shadow-lg rounded-[20px] overflow-hidden h-[450px]"> {/* Fixed height for the card */}
             {/* Image Section */}
-            {/* <div className="bg-cover bg-center h-48 w-full" style={{ backgroundImage: `url(${assetImage})` }}>
-                {/* Empty div for image */}
-            {/* </div> */}
-
-            <div className="h-70 w-full overflow-hidden">
-                <img src={assetImage} alt="Asset" className="w-full h-full object-cover" />
+            <div className="h-48 w-full overflow-hidden"> {/* Adjusted height for image */}
+                <img src={images.length > 0 ? images[0] : "default-image-url.jpg"} alt={name} className="w-full h-full object-cover" />
             </div>
 
             {/* Details Section */}
             <div className="p-6 bg-[#2B2B2B] text-white flex flex-col justify-between flex-grow">
-                <h1 className="text-2xl font-sans font-semibold">{assetName}</h1>
-                <div className="flex items-center my-2">
-                    <img src={owner.profile_img} alt="Owner" className="h-6 w-6 rounded-full mr-2"/>
-                    <p className="font-sans">{owner.owner_name}</p>
-                </div>
+                <h1 className="text-2xl font-sans font-semibold">{name}</h1>
                 <div className="my-3 flex justify-between">
                     <div>
-                        <span className="opacity-50 font-mono font-thin">Price</span>
-                        <p className="font-light pt-1">{price} ETH</p>
+                        <span className="opacity-50 font-mono font-thin">Total Supply</span>
+                        <p className="font-light pt-1">{totalSupply.toLocaleString()} Units</p>
                     </div>
-                    {/* TODO: connect the correct link */}
-                    <a href="/" className="opacity-50 font-mono font-thin pt-6">See Details</a>
+                    <div>
+                        <span className="opacity-50 font-mono font-thin">Price</span>
+                        <p className="font-light pt-1">{tokenPrice.toLocaleString()} ETH</p>
+                    </div>
                 </div>
+                {/* TODO: connect the correct link */}
+                <a href="/" className="opacity-50 font-mono font-thin pt-6">See Details</a>
             </div>
         </div>
     );
