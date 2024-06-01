af3c77bd775300826e4c9decf2be44ffb6a9b5eb
jebadeju <jebessa@itrucked.com>
2024-06-01 14:42:53 +0300

    change dist file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  build: {
+    outDir: './dist',
+    // Set the base path here if necessary
+    base: './', // Adjust based on your deployment scenario
+  },
   plugins: [react()],
 })
